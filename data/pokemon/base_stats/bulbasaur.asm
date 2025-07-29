1e1af5668e6b3897fd2ae883b115e2192a86675b
vht3036 <156178809+vht3036@users.noreply.github.com>
2025-07-29 15:17:07 +0100

    Update bulbasaur.asm
    changed gender ratio

--- data/pokemon/base_stats/bulbasaur.asm
+++ data/pokemon/base_stats/bulbasaur.asm
@@ -5,7 +5,7 @@
 	db 45 ; catch rate
 	db 64 ; base exp
 	db NO_ITEM, NO_ITEM ; held items
-	dn GENDER_F12_5, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F50, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
 
 	abilities_for BULBASAUR, OVERGROW, CHLOROPHYLL, EFFECT_SPORE
 	db GROWTH_MEDIUM_SLOW ; growth rate
